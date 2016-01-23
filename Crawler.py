ccb1021615cb88a7499e28528b43352d963cbc4e
Ellen O'Connor <eoconnor9484@gmail.com>
2016-01-23 12:59:29 -0800

    Add functionality to the job title/education scraper.

--- Crawler.py
+++ Crawler.py
@@ -1,19 +1,29 @@
-
 from bs4 import BeautifulSoup
 import urllib2
 import csv
 
-# import os
-
 class JobCrawler():
 
-    def __init__(self):
-        self.link_element = 'app_link'
-        self.next_element = 'instl confirm-nav next'
-        self.root_url = 'http://www.indeed.com'
+    def __init__(self, link_element='app_link', 
+                       next_element='instl confirm-nav next',
+                       root_url = 'http://www.indeed.com'):
+        self.link_element = link_element
+        self.next_element = next_element
+        self.root_url = root_url
 
-    def get_links(self, url):
-        """Given some url, gets all of the links to resumes on the page."""
+
+    def get_soup(self, url):
+        """Makes a request for a URL and parses the response into soup."""
+
+        request = urllib2.Request(url, headers={'User-Agent':'Mozilla/5.0'})
+        response = urllib2.urlopen(request)
+        soup = BeautifulSoup(response, 'html.parser')
+
+        return soup
+
+
+    def get_links(self, soup):
+        """Gets all of the links to resumes from some HTML."""
 
         target_elements = soup.find_all(class_ = self.link_element)
         hrefs = []
@@ -25,13 +35,14 @@ class JobCrawler():
 
 
     def crawl(self, url, results=[]):
-        """Gets all the resume urls recursively from a given starting point"""
+        """Gets all the resume urls recursively from a given starting point
+        on the search page."""
 
-        response = urllib2.urlopen(url)
-        soup = BeautifulSoup(response, 'html.parser')
+        # Parse the html for the URL 
+        soup = self.get_soup(url)
 
         # Get all the links on the page
-        results.extend(self.get_links(url))
+        results.extend(self.get_links(soup))
 
         # Look for a next button
         next_btn = soup.find(class_ = self.next_element)
@@ -52,8 +63,7 @@ class JobCrawler():
 
         for url in urls:
 
-            response = urllib2.urlopen(url)
-            soup = BeautifulSoup(response)
+            soup = self.make_soup(url)
 
             work_descriptions = self.get_work_experience(soup)
             education = self.get_education(soup)
@@ -61,11 +71,11 @@ class JobCrawler():
 
             resumes[resume_id] = {'work': work_descriptions, 
                                   'education': education,
-                                  'other': skills
-                                  }
+                                  'other': skills}
             resume_id += 1
 
         return resumes
+
 
     def get_work_experience(self, soup):
         """Given soupified html, returns relevant data associated with 
@@ -76,11 +86,20 @@ class JobCrawler():
         # For each of the listings, save the job title, the job description
         # and the order of elements (chronologically last = 1, etc.)
 
-        # Insert into csv: 
-            # anonymous resume ID 
-            # title
-            # description
-            # order within resume 
+        work_divs = soup.find_all(class_ = 'work-experience-section')
+        jobs = []
+        order = 1
+
+        if work_divs:
+            for job in work_divs:
+                title = job.find(class_ = 'work_title').get_text()
+                company = job.find(class_ = 'bold').get_text()
+                text = job.find(class_ = 'work_description').get_text()
+                jobs.append((order, title, company, text))
+                order += 1
+
+        return jobs
+
 
     def get_education(self, soup):
         """Takes in soupified html and returns the degrees listed on the 
@@ -91,21 +110,28 @@ class JobCrawler():
 
         if education_divs:
             for degree in education_divs:
-                title = degree.find(class_ = 'edu_title').string
-                school = degree.find(class_ = 'bold').string
+                title = degree.find(class_ = 'edu_title')
+                school = degree.find(class_ = 'bold')
                 degrees.append((title, school))
 
         return degrees
 
-    def get_skills(self, soup):
 
+    # def get_skills(self, soup):
 
+    #     skills = soup.find(class_ = 'skill_text').string
+    #     other = soup.find(class_ = 'additionalInfo-content')
 
+    #     text = ""
 
+if __name__ == '__main__':
 
-
-
-
+    test = JobCrawler()
+    soup = test.get_soup('http://www.indeed.com/r/Anwar-Akbar/347f76578f1dd1b3')
+    work_exp = test.get_work_experience(soup)
+    print work_exp
+    # education = test.get_education(soup)
+    # print education
 
 
 
